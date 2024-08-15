7d0c1b4ba448b6e815d25cd6c7b7189a36128d35
Petro Honcharov <pitergon@gmail.com>
2024-08-15 19:17:39 +0300

    Add .env

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
@@ -14,7 +15,6 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 
 class SearchApp:
@@ -47,13 +47,12 @@ class SearchApp:
         self.app.get("/history")(self.history_endpoint)
         self.app.post("/save_search")(self.save_search_endpoint)
 
-
-        self.connection = psycopg2.connect(user="postgres",
-                              password="mysecretpassword",
-                              host="127.0.0.1",
-                              port="5432",
-                              database="alisearch")
-        
+        load_dotenv(os.path.join(BASE_DIR, '.env'))
+        self.connection = psycopg2.connect(user=os.getenv('DB_USER'),
+                                           password=os.getenv('DB_PASSWORD'),
+                                           host=os.getenv('DB_HOST'),
+                                           port=os.getenv('DB_PORT'),
+                                           database=os.getenv('DB_DATABASE'))
 
     async def index_endpoint(self, request: Request):
         # "/"
@@ -86,7 +85,7 @@ class SearchApp:
         if not self.is_running:
             response["search_finished"] = True
             async with self.results_lock:
-                response["results"] = self.results               
+                response["results"] = self.results
         return response
 
     async def search_id_endpoint(self, request: Request, search_id):
@@ -103,16 +102,16 @@ class SearchApp:
 
         if result:
             messages_html, results_html, names_list1, names_list2 = result
-        else:   
+        else:
             messages_html = results_html = names_list1 = names_list2 = None
 
         return self.templates.TemplateResponse("search.html", {
-            "request": request, 
+            "request": request,
             "messages_html": messages_html,
             "results_html": results_html,
-            "names_list1": names_list1, 
+            "names_list1": names_list1,
             "names_list2": names_list2
-            })
+        })
 
     async def save_search_endpoint(self, page_data: SearchPageData):
         if not page_data.list1 or not page_data.list2:
@@ -122,11 +121,11 @@ class SearchApp:
         VALUES (%s,%s,%s,%s) 
         RETURNING search_id
         """
-        values = (page_data.messages_html, page_data.results_html,page_data.list1, page_data.list2)
+        values = (page_data.messages_html, page_data.results_html, page_data.list1, page_data.list2)
         with self.connection.cursor() as cursor:
             cursor.execute(postgres_insert_query, values)
             last_inserted_id = cursor.fetchone()[0]
-        
+
         self.connection.commit()
 
         return {"messages": "Saved sucessfully", "search_id": last_inserted_id}
@@ -143,14 +142,10 @@ class SearchApp:
         for record in history:
             record['created_at'] = record['created_at'].strftime('%Y-%m-%d %H:%M:%S')
 
-
-
         return self.templates.TemplateResponse("history.html", {
-            "request": request, 
+            "request": request,
             "history": history,
-            })
-    
-
+        })
 
     async def _prepare_search(self):
         self.is_running = True
@@ -162,13 +157,12 @@ class SearchApp:
         async with self.messages_lock:
             self.se.messages.clear()
 
-
-    async def _create_search_task(self, query_list: list[tuple]):       
+    async def _create_search_task(self, query_list: list[tuple]):
         search_task = asyncio.create_task(self._run_search(query_list))
         self.background_tasks.add(search_task)
         search_task.add_done_callback(self.background_tasks.discard)
 
-    async def _run_search(self, query_list: list[tuple]):            
+    async def _run_search(self, query_list: list[tuple]):
         results = await self.se.intersection_in_global_search(query_list)
         async with self.results_lock:
             self.results = results
@@ -180,9 +174,10 @@ class SearchApp:
         self.read_messages.extend(new_messages)
         return new_messages
 
-    def __del__(self):       
+    def __del__(self):
         if self.connection:
             self.connection.close()
+
 
 search_app = SearchApp()
 app = search_app.app
